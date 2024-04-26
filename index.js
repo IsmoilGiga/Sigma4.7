91dc0ec1a1e7149b40ad0bf86173a3f68732db7f
IsmoilGiga <ismoilagzamovman@gmail.com>
2024-04-26 11:44:47 +0500

    Sigma4.7

---
+++ index.js
@@ -0,0 +1,14 @@
+let ismoil = ["Ismoil", 12, true, "Karima"];
+
+console.log(ismoil)
+console.log(ismoil.length)
+
+let rost = confirm("Manziling'iz to'g'rimi?");
+
+if (rost) {
+
+} else {
+    ismoil.pop()
+}
+
+console.log(ismoil)
